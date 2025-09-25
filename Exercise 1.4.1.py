386abc4a573a99805314dd22478119c69d4715c1
Cave <a@a.com>
2025-09-25 21:27:25 +0200

    Let's get down to work

---
+++ Exercise 1.4.1.py
@@ -0,0 +1,70 @@
+﻿
+#1. Create a list of 10 famous artists and print the list.
+
+artists_raw_list = '''
+Leonardo da Vinci
+Monet
+Vincent van Gogh
+Picasso
+Rembrandt
+Dali
+Michelangelo
+Renoir
+Chagall
+Matisse
+'''
+
+artists = artists_raw_list.splitlines()
+
+artists = list(filter(None, artists)) # Remove empty strings
+
+# *!*  filter(None, iterable) - remain only 'True' elements 
+
+print( f"I've found {len(artists)} artists:\n{artists}" )
+
+#2. Add a famous artist to the list of artists and print the updated list.
+
+artists.append('Banksy')
+print(artists)
+
+#3. Order the list alphabetically and print the three first artists.
+
+artists.sort(key=str.lower)
+
+# *!* sort() modifies the original list!
+# *!* sorted() returns a new list!
+
+print(artists[:])
+print(artists[0:3])
+
+#4. Create a tuple containing three famous artworks and print the tuple.
+
+artworks_raw_list = '''Mona Lisa
+Starry Night
+The Persistence of Memory
+'''
+
+artworks_list = artworks_raw_list.splitlines()
+
+artworks_tuple = tuple(artworks_list)
+
+print("\n" * 5) # Print vert.space
+
+print(artworks_tuple)
+
+# Add element to immutable tuple
+
+artworks_tuple = artworks_tuple + ('Guernica',)
+
+print(artworks_tuple)
+
+#5. Create a set of art styles
+#7. Create a second set of art styles and print the common elements.
+
+
+
+#8. Create a dictionary with famous artists as keys and their most known artwork as values.
+#9. Add a new artist and his/her most famous artwork to the existing dictionary and print it.
+#10. Access the most famous artwork of a specific artist in the dictionary and print it.
+#11. Remove an artist and his/her artwork from the dictionary and print the updated dictionary.
+
--- Tasks.md
+++ Tasks.md
@@ -1,1 +1,15 @@
-#### Pre-training Tasks
+<h3 align="center"> Pre-training Tasks</h3>
+
+#### Exercises - Part 1 - Notebook 4
+
+1. Create a list of 10 famous artists and print the list.
+2. Add a famous artist to the list of artists and print the updated list.
+3. Order the list alphabetically and print the three first artists.
+4. Create a tuple containing three famous artworks and print the tuple.
+5. Create a set of art styles and print the set.
+6. Add an art style to an existing set of art styles and print the updated set.
+7. Create a second set of art styles and print the common elements.
+8. Create a dictionary with famous artists as keys and their most known artwork as values.
+9. Add a new artist and his/her most famous artwork to the existing dictionary and print it.
+10. Access the most famous artwork of a specific artist in the dictionary and print it.
+11. Remove an artist and his/her artwork from the dictionary and print the updated dictionary.
