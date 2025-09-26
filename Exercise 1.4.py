d2c07a5a5f9582c57e6742dc928997c37f448f81
Cave <a@a.com>
2025-09-26 22:42:32 +0200

    Exercise 1.4

--- Exercise 1.4.py
+++ Exercise 1.4.py
@@ -61,10 +61,54 @@ print(artworks_tuple)
 #5. Create a set of art styles
 #7. Create a second set of art styles and print the common elements.
 
+artstyles_old = {'Baroque','Realism','Minimalism','Impressionism','Expressionism','Cubism','Futurism'}
 
+artsyles_new = {'Surrealism','Abstract Art','Pop Art','Minimalism','Futurism','Realism'}
+
+print( f"Common elements in styles: {artstyles_old.intersection(artsyles_new)}" )
 
 #8. Create a dictionary with famous artists as keys and their most known artwork as values.
 #9. Add a new artist and his/her most famous artwork to the existing dictionary and print it.
 #10. Access the most famous artwork of a specific artist in the dictionary and print it.
 #11. Remove an artist and his/her artwork from the dictionary and print the updated dictionary.
+
+# Create: 
+artworks = {}
+
+artworks['Leonardo da Vinci']='Mona Lisa'
+artworks['Vincent van Gogh']='Starry Night'
+
+# Add with checking:
+artist = 'Rembrandt'
+artwork='The Night Watch'
+
+if(artist not in artworks):
+    artworks[artist] = artwork
+
+artist = 'Pablo Picasso'
+artwork='Guernica'
+
+# Add with default value:
+artworks.setdefault(artist,artwork) 
+artworks.setdefault(artist,'Unknown art') # Nothing to do if key artist exists
+
+artworks['Claude Monet'] = 'forgery'
+# Ups... Sorry, I’ll delete it now:
+del artworks['Claude Monet']
+
+print('\n'*3,artworks)
+
+# find by value:
+value = 'Starry Night2'
+found_keys = [k for (k, v) in artworks.items() if v == value]
+
+if(len(found_keys)>1):
+    pass
+elif(len(found_keys)==0):
+    raise KeyError("Key not found")
+else:
+    print( f'Artist {found_keys[0]} draw {value}' )
+
+###################################################
+
 
