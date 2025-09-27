fe8b0fc09cb4f8ceb3a2925c7a1bfb1214a51234
Cave <a@a.com>
2025-09-27 14:34:08 +0200

    Ex. 1.5

---
+++ Exercise 1.5.py
@@ -0,0 +1,60 @@
+﻿
+# Determine if a number is even or odd
+
+x = 50
+
+assert type(x) is int, "var. x must be int"
+
+if( x%2 == 0 ):
+    print('Even')
+else:
+    print('Odd')    
+
+
+# Print the largest number among 3 numbers
+
+a = 3
+b = 3
+c = 3
+
+if( a>=b and a>=c ): 
+    print(a)
+elif( b>=a and b>=c ): 
+    print(b)
+elif c>=a and c>=b : 
+    print(c)
+else: 
+    raise RuntimeError("Algorithmic error")
+    
+
+# Check wheter a letter is a vowel or a consonant
+
+char = 'i'
+
+vowel = ('a','e','i','o','u''A','E','I','O','U')
+
+assert len(char)==1 and type(char) is str
+
+if( char in vowel ):
+    print('vowel')
+else:
+    print('consonant')    
+
+# Create a Python program that checks if a given character 
+# is an uppercase letter, a lowercase letter, a digit, or a special character
+
+ch = 'с'
+
+if( ch.islower() ):
+    print('lowercase')
+
+if( ch.isupper() ):
+    print('uppercase')
+    
+if( ch.isdigit() ):
+    print('digit')
+
+if not ch.isalnum():
+    print('special character')
+
+#######################
--- Tasks.md
+++ Tasks.md
@@ -1,6 +1,6 @@
 <h3 align="center"> Pre-training Tasks</h3>
 
-#### Exercises - Part 1 - Notebook 4
+#### Part 1 - Notebook 4
 
 1. Create a list of 10 famous artists and print the list.
 2. Add a famous artist to the list of artists and print the updated list.
@@ -13,3 +13,11 @@
 9. Add a new artist and his/her most famous artwork to the existing dictionary and print it.
 10. Access the most famous artwork of a specific artist in the dictionary and print it.
 11. Remove an artist and his/her artwork from the dictionary and print the updated dictionary.
+
+#### Part 1 - Notebook 5
+
+1. Determine if a number is even or odd
+2. Print the largest number among 3 numbers
+3. Check wheter a letter is a vowel or a consonant
+4. Create a Python program that checks if a given character is an uppercase letter, a lowercase letter, a digit, or a special character
+
