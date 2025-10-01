5313133ee91f9077da09162c8802cf23cfac4263
Cave <a@a.com>
2025-10-01 22:42:30 +0200

    first function from part II

--- Part II/my_functions.py
+++ Part II/my_functions.py
@@ -1,11 +1,30 @@
-﻿ 
+﻿
+#################################
+ 
 def multiplication(x, y):
     return x*y
 
 assert multiplication(3,5)==15
 
+#################################
 
+# Take a string as input and return `True` if the string 
+# is a palindrome (reads the same backward and forward) 
+# and `False` otherwise.
 
+def is_palindrome(text:str):
+    
+    chars = list(text)
+    
+    chars.reverse()
+    
+    reversed_text = ''.join(chars)
+    
+    return ( True if text == reversed_text else False )
+    
+    
+assert is_palindrome('ABC')==False
+assert is_palindrome('ASDF5FDSA')==True
 
 #################################
 
--- Part II/my_functions_test.py
+++ Part II/my_functions_test.py
@@ -6,6 +6,25 @@ import pytest
 from my_functions import *
 
 ########################################
+########################################
+########################################
+
+def test_is_palindrome():
+    assert is_palindrome('Abdbt') == False
+    assert is_palindrome('asdf4fdsa') == True
+    assert is_palindrome('asdffdsa') == True
+    assert is_palindrome('WWW') == True
+    assert is_palindrome('WW') == True
+    assert is_palindrome('W') == True
+    assert is_palindrome('') == True
+
+
+
+
+
+########################################
+
+# Try to use @pytest.mark.parametrize #
 
 test_data_multiplication = [
     (2, 2, 4),
@@ -18,6 +37,8 @@ test_data_multiplication = [
 def test_multiplication(x,y,expected):
     assert multiplication(x, y) == expected
 
+########################################
+########################################
 ########################################
 
 if __name__ == "__main__":
